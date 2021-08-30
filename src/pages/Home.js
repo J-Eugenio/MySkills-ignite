d9c238e9dbb7941312155786de1b7c44c46f4b2c
j-eugenio <eugeniopb4@gmail.com>
2021-08-30 14:02:41 -0300

    stylesheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,72 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  TextInput,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
 
 export function Home(){
   return (
-    <View style={styles.container}>
-      <Text>React Native</Text>
-      <Text>Ignite</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Welcome, Eugenio
+      </Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder='New skill'
+        placeholderTextColor='#555'
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My Skills
+      </Text>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+    backgroundColor: '#121015', 
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold'
   }
 })
